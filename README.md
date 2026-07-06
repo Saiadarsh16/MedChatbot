44e3d26c004eb93746b2081a29128e73e8f60af1
Sai Adarsh Malla <pydisaiadarshmalla@gmail.com>
2026-07-06 09:48:28 -0400

    setup medibot environment and install dependencies

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MedChatbot
+
+
+
+
+Clone the repository
+
+```bash
+git clonehttps://github.com/entbappy/Build-a-Complete-Medical-Chatbot-with-LLMs-LangChain-Pinecone-Flask-AWS.git
+```
+
+```bash
+conda create -n medibot python=3.10 -y
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="Adarsh Malla",
+    author_email="adarshmalla16@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
